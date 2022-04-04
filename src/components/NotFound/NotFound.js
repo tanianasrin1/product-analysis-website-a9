a9bb34f2e0a09e826ead6d156db79d2fbf32f99a
tanianasrin1 <tasrineva169@gmail.com>
2022-04-04 22:45:07 +0600

    Not found fixed

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
         <div>
-            This is a notFound page
+            <div className='NotFound-body'>
+            <h1>This is a NotFound page : </h1>
+            <h1 className='NotFound-title'>404</h1>
+            </div>
+            
         </div>
     );
 };
