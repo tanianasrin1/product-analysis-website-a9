36cb28d1735a3589ecda1d1f13490918a02c986e
tanianasrin1 <tasrineva169@gmail.com>
2022-04-05 00:06:56 +0600

    chart page fixed

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import {  Area, AreaChart, Bar, BarChart, CartesianGrid, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import './DashBoard.css'
 
 const DashBoard = () => {
@@ -9,37 +9,43 @@ const DashBoard = () => {
             "investment": 100000,
             "sell": 241,
             "revenue": 10401,
+            "name": 'Page A'
             
         },
         {
             "month": "Apr",
             "investment": 200000,
             "sell": 423,
-            "revenue": 24500
+            "revenue": 24500,
+            "name": 'Page B'
         },
         {
             "month": "May",
             "investment": 500000,
             "sell": 726,
-            "revenue": 67010
+            "revenue": 67010,
+            "name": 'Page C'
         },
         {
             "month": "Jun",
             "investment": 500000,
             "sell": 529,
-            "revenue": 40405
+            "revenue": 40405,
+            "name": 'Page D'
         },
         {
             "month": "Jul",
             "investment": 600000,
             "sell": 601,
-            "revenue": 50900
+            "revenue": 50900,
+            "name": 'Page E'
         },
         {
             "month": "Aug",
             "investment": 700000,
             "sell": 670,
-            "revenue": 61000
+            "revenue": 61000,
+            "name": 'Page F'
         }
     ]
 
@@ -64,32 +70,47 @@ const DashBoard = () => {
       ];
 
     return (
-        <div className='rechart'>
+        <div  >
             
-          <div>
-              
-          <LineChart width={450} height={200} data={data}>
-                <Line dataKey={"sell"}></Line>
-                <XAxis dataKey={"month"}></XAxis>
-                <Tooltip></Tooltip>
-                <YAxis></YAxis>
-
-            </LineChart>
-            <h2>Investment vs Revenue</h2>
-          </div>
-            
-       
+           <div className='rechart'>
            <div>
-               
-           <PieChart width={400} height={400}>
-                <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8"></Pie>
-                <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label ></Pie>
-
-                <Tooltip></Tooltip>
-
-            </PieChart>
-            <h2>Investment vs Revenue</h2>
+              
+              <LineChart width={450} height={200} data={data}>
+                    <Line dataKey={"sell"}></Line>
+                    <XAxis dataKey={"month"}></XAxis>
+                    <Tooltip></Tooltip>
+                    <YAxis></YAxis>
+    
+                </LineChart>
+                <h2>Investment vs Revenue</h2>
+              </div>
+                
+           
+               <div>
+                   
+               <PieChart width={400} height={400}>
+                    <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8"></Pie>
+                    <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label ></Pie>
+    
+                    <Tooltip></Tooltip>
+    
+                </PieChart>
+                <h2>Investment vs Revenue</h2>
+               </div>
            </div>
+
+
+          <div className='rechart'>
+          <div >
+                   <BarChart width={250} height={400} data={data}>
+                       <Bar dataKey="revenue" fill="#8884d8"></Bar>
+                       <Tooltip></Tooltip>
+                   </BarChart>
+                   <h2>Investment vs Revenue</h2>
+           </div>
+          </div>
+
+          
             
         </div>
     );
