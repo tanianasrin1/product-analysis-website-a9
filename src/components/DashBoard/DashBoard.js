bb569045bfccf70fd6e8d10cb9061063698cdf06
tanianasrin1 <tasrineva169@gmail.com>
2022-04-04 11:48:21 +0600

    chart fixed

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const DashBoard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
     return (
         <div>
-            This is Dashboard
+            <h4>nbhjnj</h4>
+            <LineChart width={450} height={200} data={data}>
+                <Line dataKey={"sell"}></Line>
+                <XAxis dataKey={"month"}></XAxis>
+                <Tooltip></Tooltip>
+                <YAxis></YAxis>
+
+            </LineChart>
+            
+            
+
         </div>
     );
 };
