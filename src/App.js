7205bab0531b00ce74c4fdeb2324a306745b9b66
tanianasrin1 <tasrineva169@gmail.com>
2022-04-04 10:01:17 +0600

    added body

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Route path='/dashboard' element= {<DashBoard></DashBoard>}></Route>
         <Route path='/blogs' element= {<Blogs></Blogs>}></Route>
         <Route path='/about' element= {<About></About>}></Route>
-        <Route path='*' element= {<NotFound></NotFound>}></Route>
+        {/* <Route path='*' element= {<NotFound></NotFound>}></Route> */}
       </Routes>
     </div>
   );
