92e907550ddb3918bcbee0e45049483e923054f3
tanianasrin1 <tasrineva169@gmail.com>
2022-04-04 12:29:22 +0600

    chart fixed

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './DashBoard.css'
 
 const DashBoard = () => {
     const data = [
@@ -7,7 +8,10 @@ const DashBoard = () => {
             "month": "Mar",
             "investment": 100000,
             "sell": 241,
-            "revenue": 10401
+            "revenue": 10401,
+            // "cx":"50%",
+            // "cy":"50%",
+            // "outerRadius":50
         },
         {
             "month": "Apr",
@@ -40,9 +44,30 @@ const DashBoard = () => {
             "revenue": 61000
         }
     ]
+
+    const data01 = [
+        { name: 'Group A', value: 400 },
+        { name: 'Group B', value: 300 },
+        { name: 'Group C', value: 300 },
+        { name: 'Group D', value: 200 },
+      ];
+      const data02 = [
+        { name: 'A1', value: 100 },
+        { name: 'A2', value: 300 },
+        { name: 'B1', value: 100 },
+        { name: 'B2', value: 80 },
+        { name: 'B3', value: 40 },
+        { name: 'B4', value: 30 },
+        { name: 'B5', value: 50 },
+        { name: 'C1', value: 100 },
+        { name: 'C2', value: 200 },
+        { name: 'D1', value: 150 },
+        { name: 'D2', value: 50 },
+      ];
+
     return (
-        <div>
-            <h4>nbhjnj</h4>
+        <div className='rechart'>
+            
             <LineChart width={450} height={200} data={data}>
                 <Line dataKey={"sell"}></Line>
                 <XAxis dataKey={"month"}></XAxis>
@@ -50,8 +75,16 @@ const DashBoard = () => {
                 <YAxis></YAxis>
 
             </LineChart>
+
+            <PieChart width={400} height={400}>
+                <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8"></Pie>
+                <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label ></Pie>
+
+                <Tooltip></Tooltip>
+
+            </PieChart>
             
-            
+
 
         </div>
     );
