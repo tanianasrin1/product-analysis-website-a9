429edf3376016f2f311a79bfb087376a24f5e1f1
tanianasrin1 <tasrineva169@gmail.com>
2022-04-04 11:00:28 +0600

    added body

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
@@ -12,14 +11,15 @@ import NotFound from "./components/NotFound/NotFound";
 function App() {
   return (
     <div className="App">
-     
+      {/* <h3>b nbh</h3> */}
+      <Header></Header>
       <Routes>
         <Route path= '/' element={<Home></Home>}></Route>
         <Route path='/review' element= {<Review></Review>}></Route>
         <Route path='/dashboard' element= {<DashBoard></DashBoard>}></Route>
         <Route path='/blogs' element= {<Blogs></Blogs>}></Route>
         <Route path='/about' element= {<About></About>}></Route>
-        {/* <Route path='*' element= {<NotFound></NotFound>}></Route> */}
+        <Route path='*' element= {<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css'
 
 const Header = () => {
-    return (   
-        <nav>
-          <Link to = '/'>Home</Link> 
-          <Link to = '/review' >Review</Link>  
-          <Link to = '/dashboard' >DashBoard</Link>  
-          <Link to = '/blogs' >Blogs</Link>  
-          <Link to ='/about' >About</Link>
-     
-        </nav>
-    );
+  return (
+    <div  >
+      <nav>
+        <CustomLink to='/' >Home</CustomLink>
+        <CustomLink to='/review' >Reviews</CustomLink>
+        <CustomLink to='/dashboard' >Dashboard</CustomLink>
+        <CustomLink to='/blogs' >Blogs</CustomLink>
+        <CustomLink to='/about' >About</CustomLink>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
